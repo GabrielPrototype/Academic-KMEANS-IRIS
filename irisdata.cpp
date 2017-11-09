282522d5bc497186b146013bb36a8c81499b8d67
Gabriel L. P. Abreu <gabriel.longo.longo@gmail.com>
2017-11-09 00:49:35 -0200

    Added euclidianDistance to calc distance between the four dimensions of IrisDataItem

--- irisdata.cpp
+++ irisdata.cpp
@@ -44,6 +44,23 @@ IrisDataItem IrisData::getItem(std::int32_t pos)
     irisItem[pos];
 }
 
+double IrisData::euclidianDistance(IrisDataItem iris1, IrisDataItem iris2)
+{
+
+    double a1, a2, a3, a4;
+    double dist;
+
+    a1 = iris1.getPetalLengthCm() - iris2.getPetalLengthCm();
+    a2 = iris1.getPetalWidthCm() - iris2.getPetalWidthCm();
+    a3 = iris1.getSepalLengthCm() - iris2.getSepalLengthCm();
+    a4 = iris1.getSepalWidthCm() - iris2.getSepalWidthCm();
+
+    dist = std::pow(a1,2) + std::pow(a2,2) + std::pow(a3,2) + std::pow(a4,2);
+    dist = std::sqrt(dist);
+
+    return dist;
+}
+
 std::vector<IrisDataItem> IrisData::getIrisVector() const
 {
     return irisItem;
@@ -51,7 +68,7 @@ std::vector<IrisDataItem> IrisData::getIrisVector() const
 
 void IrisData::replaceItem(int32_t pos, IrisDataItem &value)
 {
-    if(pos <= irisItem.size() && pos >= 0)
+    if(pos < irisItem.size() && pos >= 0)
         irisItem.insert(irisItem.begin(),value);
 }
 
